edb4e483764278742294d033756302dc2ef6bba8
Aurora Aaltonen <Aurora.Aaltonen@kukkula.local>
2025-08-12 14:12:35 +0300

    update

--- cms/admin.php
+++ cms/admin.php
@@ -1,6 +1,6 @@
 <?php
 // Connect to the database
-$conn = new mysqli("localhost", "root", "", "cms");
+$conn = new mysqli($_ENV["DB_HOST"], $_ENV["DB_USERNAME"], $_ENV["DB_PASSWORD"], $_ENV["DB_NAME"]);
 
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
